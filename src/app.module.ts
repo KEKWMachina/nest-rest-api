38efc66c9ab6c678cde72736b228d4c0da2a7d70
Maksym Rudenko <maksym.rudenko@eleks.com>
2023-05-30 00:20:48 +0300

    [Mongo DB Setup] Added MongoDB CRUD controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,22 @@ import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EmployeeModule } from './employee/employee.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PostsModule, UsersModule, AuthModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PostsModule,
+    UsersModule,
+    AuthModule,
+    EmployeeModule,
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_ACCESS_NAME}:${process.env.DB_ACCESS_PASSWORD}@cluster0.fbzuyvk.mongodb.net/?retryWrites=true&w=majority`,
+    ),
+  ],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { jwtConstants } from './auth/auth.constants';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '600s' },
+      signOptions: { expiresIn: '7200s' },
     }),
   ],
   providers: [AuthService],
