f9f9ebae55c50e975a7d4b31f11a353f13baf56e
Maksym Rudenko <maksym.rudenko@eleks.com>
2023-05-30 00:30:06 +0300

    [JWT] Hid JWT secret into .env file

--- src/auth/auth/auth.constants.ts
+++ src/auth/auth/auth.constants.ts
@@ -1,4 +1,3 @@
 export const jwtConstants = {
-  secret:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjExLCJwYXNzd29yZCI6InRlc3QxIiwiaWF0IjoxNjg1MTMxMzA2fQ.8X8FdRMZ3cSLa99TmeMwwK-jJSMFuOyl5CGkc4AJxH0',
+  secret: process.env.WEB_TOKEN_KEY,
 };
